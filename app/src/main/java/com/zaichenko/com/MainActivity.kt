98d8fde5b39a95653b22e7409cc4d4eae86ebce1
duxa4854 <nzaichenko531@gmail.com>
2026-03-06 10:36:14 +0300

    Initial commit

---
+++ app/src/main/java/com/zaichenko/com/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.zaichenko.com
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zaichenko.com.ui.theme.Laba1011Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laba1011Theme {
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laba1011Theme {
+        Greeting("Android")
+    }
+}
